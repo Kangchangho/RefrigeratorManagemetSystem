813f5fcac3d4c16829937f6c91bb134782c3cd67
user <user@182.31.45.100>
2020-03-19 21:26:41 +0900

    create file

--- src/RefrigeratorManagemetSystem.java
+++ src/RefrigeratorManagemetSystem.java
@@ -2,7 +2,14 @@
 public class RefrigeratorManagemetSystem {
 
 	public static void main(String[] args) {
-		System.out.println("gig");
+		
+		System.out.println("Refrigerator Managemet System");
+		System.out.println("1. Add food");
+		System.out.println("2. Delete food");
+		System.out.println("3. Edit food");
+		System.out.println("4. Veiw food");
+		System.out.println("5. Exit");
+		System.out.println("Select one number between 1 - 5");
 
 	}
 
