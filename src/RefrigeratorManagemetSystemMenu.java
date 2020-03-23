210e02ca64832717c08a5f6183d769c82f6176c1
user <user@182.31.45.100>
2020-03-23 19:58:16 +0900

    f

--- src/RefrigeratorManagemetSystemMenu.java
+++ src/RefrigeratorManagemetSystemMenu.java
@@ -1,9 +1,9 @@
 
-public class RefrigeratorManagemetSystem {
+public class RefrigeratorManagemetSystemMenu {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Refrigerator Managemet System");
+		System.out.println("Refrigerator Managemet System Menu");
 		System.out.println("1. Add food");
 		System.out.println("2. Delete food");
 		System.out.println("3. Edit food");
